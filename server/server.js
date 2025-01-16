286bfb6b7b11f660ca54c4161ae8c6fbc342629d
Thapelo Somo <edwardthapelo55@gmail.com>
2025-01-16 23:49:47 +0200

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,12 +1,35 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const weatherRoutes = require("./routes/weather");
+const favoritesRoutes = require("./routes/favorites");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('react')
-});
+const corsOptions = {
+  origin: ["http://localhost:5000", "http://localhost:5173"], 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true // Enable if you're using cookies/sessions
+};
 
-app.listen(3000, () =>{
-    console.log("Server Online")
-    console.log("Likewise")
-})
+// Middleware
+app.use(cors(corsOptions.origin));
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/weather", weatherRoutes);
+app.use("/api/favorites", favoritesRoutes);
+
+// Export the app as a serverless function handler
+module.exports = (req, res) => {
+  app(req, res);
+};
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
